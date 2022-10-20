94ca2db3dc7ad48e8f9231b37f86dc66af8096b4
Artemii Dovhopolyi <dovgopoly123@gmail.com>
2022-10-20 21:52:11 +0300

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,7 @@
-class Utils {
-    static randomIntFromRange(min: number, max: number) : number {
-        return Math.floor(Math.random() * (max - min + 1) + min)
-    }
-
-    static pick_random_thing<T>(ma_stuff: Array<T>) : T{
-        return ma_stuff[Math.floor(Math.random() * ma_stuff.length)]
-    }
+export function randomIntFromRange(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-export default Utils;
+export function pickRandomItem<T>(array: Array<T>): T {
+        return ma_stuff[Math.floor(Math.random() * array.length)]
+}
